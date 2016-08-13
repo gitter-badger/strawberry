bc07ead7c71c1a08639c56e3050efca9e2f213c2
Maxime Gélinas <maxime.gelinas@idmobiles.com>
2016-08-13 19:17:44 -0400

    Test str-ends-with function

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@
 const path = require('path');
 const sassTrue = require('sass-true');
 
-let sassFile = path.join(__dirname, 'index.scss');
+let sassFile = path.join(__dirname, '_index.scss');
 
 sassTrue.runSass({ file: sassFile }, describe, it);
