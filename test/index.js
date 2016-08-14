a8eef1d71cbe2543c78fdde0fbcd48b04b4e451f
Maxime Gélinas <maxime.gelinas@idmobiles.com>
2016-08-14 18:35:41 -0400

    Add the test group

--- test/index.js
+++ test/index.js
@@ -5,4 +5,11 @@ const sassTrue = require('sass-true');
 
 let sassFile = path.join(__dirname, '_index.scss');
 
-sassTrue.runSass({ file: sassFile }, describe, it);
+sassTrue.runSass(
+    {
+        file: sassFile,
+        includePaths: [path.join(__dirname, '../src')]
+    },
+    describe,
+    it
+);
